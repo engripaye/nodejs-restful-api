db339d2d3ce842b15364a6cc3ad9bf7df97a441f
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-03-12 10:47:08 +0100

    corrections on db.js

--- db.js
+++ db.js
@@ -1,1 +1,18 @@
 // code to connect express application with mongodb
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://mongo:27017/productsDB', 
+        {useNewUrlParser: true, 
+        useUnifiedTopology: true
+    });
+    console.log("MongoDB Connected")
+    }
+    catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    } 
+}
+
+module.exports = connectDB;
